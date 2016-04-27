83b263efd082d194a1e99a0bba9fc3d57a1120af
SamanShafigh <samanshafigh@gmail.com>
2016-04-27 16:22:02 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+node-airwriting
+==============
+
+This project use node-fann (FANN bindings for Node.js) to develop a neural network classification system that converts user's hand movement, and particularly in our case writing patterns in the air, to the corresponding characters. User's hand movement information has been obtained by attaching a small wireless kinematic sensor (Shimmer wearable kinematics sensor) on the user's wrist. These data will be
+transmitted through Bluetooth communication to the based system (e.g. user's mobile phone) that is responsible for classifying and converting these data to the corresponding characters.
+
+
---
+++ app.js
@@ -0,0 +1,51 @@
+var fann = require('fann');
+
+var net = new fann.standard(2,8,2);
+/*
+"FANN_LINEAR",
+"FANN_THRESHOLD",
+"FANN_THRESHOLD_SYMMETRIC",
+"FANN_SIGMOID",
+"FANN_SIGMOID_STEPWISE",
+"FANN_SIGMOID_SYMMETRIC",
+"FANN_SIGMOID_SYMMETRIC_STEPWISE",
+"FANN_GAUSSIAN",
+"FANN_GAUSSIAN_SYMMETRIC",
+"FANN_GAUSSIAN_STEPWISE",
+"FANN_ELLIOT",
+"FANN_ELLIOT_SYMMETRIC",
+"FANN_LINEAR_PIECE",
+"FANN_LINEAR_PIECE_SYMMETRIC",
+"FANN_SIN_SYMMETRIC",
+"FANN_COS_SYMMETRIC",
+"FANN_SIN",
+"FANN_COS"
+*/
+net.activation_function_hidden('FANN_SIGMOID_SYMMETRIC');
+
+var data = [
+    [[0, 0], [0, 0]],
+    [[0, 1], [1, 1]],
+    [[1, 0], [1, 1]],
+    [[1, 1], [0, 0]],
+];
+net.train(data, {error: 0.00001});
+
+var result = net.run([0, 0]);
+net.save('model/fann.txt');
+console.log('\nTraining....');
+[0,1].forEach(function(a) {
+  [0,1].forEach(function(b) {
+    var c = net.run([a, b]);
+    console.log("xor test (" + a + "," + b + ") -> " + c)
+  });
+});
+
+var netLive = new fann.load('model/fann.txt');
+console.log('\nLive....');
+[0,1].forEach(function(a) {
+  [0,1].forEach(function(b) {
+    var c = netLive.run([a, b]);
+    console.log("xor test (" + a + "," + b + ") -> " + c)
+  });
+});
